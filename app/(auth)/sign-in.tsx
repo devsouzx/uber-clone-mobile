c6ca4b63b32974c3c89492729e58909e47e31b54
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-23 18:58:38 -0300

    Onboarding Screens

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SingIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>Sign In</Text>
+    </SafeAreaView>
+  );
+};
+
+export default SingIn;
